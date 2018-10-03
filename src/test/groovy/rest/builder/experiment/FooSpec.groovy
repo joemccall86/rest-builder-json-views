80527ae943114b910bf6270e5e1fce7ac731fcc5
Joe McCall <joemccall@intrepid-networks.com>
2018-10-03 09:04:40 -0400

    Add better test

--- src/test/groovy/rest/builder/experiment/FooSpec.groovy
+++ src/test/groovy/rest/builder/experiment/FooSpec.groovy
@@ -2,31 +2,42 @@ package rest.builder.experiment
 
 import grails.converters.JSON
 import grails.plugins.rest.client.RestBuilder
-import grails.validation.Validateable
+import grails.testing.spring.AutowiredTest
+import grails.validation.ValidationErrors
 import org.grails.web.converters.exceptions.ConverterException
 import spock.lang.Specification
+import spock.lang.Unroll
 
-class FooSpec extends Specification {
+class FooSpec extends Specification implements AutowiredTest {
+//class FooSpec extends Specification implements GrailsUnitTest {
 
-    void 'instantiation of RestBuilder does not poison json rendering'() {
-        given: 'an invalid command object'
-        def cmd = new UpdateLocationCmd(latitude: 30.0)
-        cmd.validate()
+    // NOTE: this must implement Autowired test for the happy-path to work.
 
-        def writer = new StringWriter()
-        def rest = new RestBuilder()
-
-        when: 'a location update is attempted without a required field'
-        (cmd.errors as JSON).writeTo(writer)
-
-
-        then: 'the error is reported'
-        notThrown(ConverterException)
-        writer.toString() != null
+    Set<String> getIncludePlugins() {
+        ["converters"].toSet()
     }
-}
 
-class UpdateLocationCmd implements Validateable {
-    BigDecimal latitude
-    BigDecimal longitude
+    @Unroll
+    void 'init rest builder #initRestBuilder does not poison JSON rendering'() {
+        given: 'an errors object to render'
+        def errors = new ValidationErrors('foo', 'bar')
+
+        and: 'a new object marshaller is registered, does not matter the class'
+        JSON.registerObjectMarshaller(Date, {
+            [message: 'this is a date']
+        })
+
+        if (initRestBuilder) {
+            def rest = new RestBuilder()
+        }
+
+        when: 'the object is rendered'
+        (errors as JSON).toString()
+
+        then: 'no exception is thrown'
+        notThrown(ConverterException)
+
+        where:
+        initRestBuilder << [false, true]
+    }
 }
